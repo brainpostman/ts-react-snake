e067e0a34d33e166307adef1f9315cd30ac1e8aa
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-04 19:15:31 +0500

    Fix (root) base path parity with GH pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './store/index.ts';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename='react-snake'>
+            <BrowserRouter basename='ts-react-snake'>
                 <AppRouter />
             </BrowserRouter>
         </Provider>
