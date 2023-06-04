82ec70880df7fb3669af0fc94c7897d98fc6f9a5
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-04 18:41:13 +0500

    Fix (root) routing fixes

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,22 +4,11 @@ import Snake from './components/Snake';
 import { useAppSelector } from './hooks/ReduxHooks';
 
 const AppRouter = () => {
-    const initialGameState = useAppSelector((state) => state.initialGameState);
-
     return (
         <Routes>
             <Route path='/' element={<Navigate to='setup' replace />}></Route>
             <Route path='setup' element={<App />} />
-            <Route
-                path='game'
-                element={
-                    initialGameState.gridInitial.length !== 0 ? (
-                        <Snake />
-                    ) : (
-                        <Navigate to='setup' replace />
-                    )
-                }
-            />
+            <Route path='game' element={<Snake />} />
         </Routes>
     );
 };
