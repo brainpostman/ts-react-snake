0a731c7aaeb3bd7d841872567709b303ccf544b7
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-04 18:42:06 +0500

    Fix (src) redux provider fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './main.css';
+import './styles/main.scss';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './Router.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter basename='react-snake'>
-            <AppRouter />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter basename='react-snake'>
+                <AppRouter />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
